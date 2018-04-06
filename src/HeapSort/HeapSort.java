0957d5379c6f8edca54ea5645ddb25679e41da42
ozavraham <ozavraham@10.0.0.5>
2018-04-06 06:05:32 +0300

    Getting started..

--- src/HeapSort/HeapSort.java
+++ src/HeapSort/HeapSort.java
@@ -1,5 +1,26 @@
 package HeapSort;
 
 public class HeapSort {
-
+	
+	public static <T extends Comparable<T>> void sort(T[] array) {
+		buildHeap(array);
+		for (int i=array.length ; i>1 ; i--) {
+			swap(array, i, 0);
+			Heapify(array, i);
+		}
+	}
+	
+	private static <T extends Comparable<T>> void buildHeap(T[] array) {
+		
+	}
+	
+	private static <T extends Comparable<T>> void Heapify(T[] array, int i) {
+		
+	}
+	
+	private static void swap(Object[] array, int i, int j) {
+		Object temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
 }
--- src/HeapSort/Main.java
+++ src/HeapSort/Main.java
@@ -1,10 +1,73 @@
 package HeapSort;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int choise;
+		boolean isValidInput = false;
+		Scanner read = new Scanner(System.in);
+		System.out.println("Welcome to HeapSort Generic Exercise!");
+		System.out.println("Please choose from the following:");
+		System.out.println("1. Integer array:");
+		System.out.println("2. String array:");
+		System.out.println("-1 to Exit.");
+		while (!isValidInput) {
+			choise = read.nextInt();
+			if (choise==1) {
+				int size = getSize();
+				Integer[] array = new Integer[size];
+				for (int i=0 ; i<size ; i++) {
+					System.out.println("Please enter value for place number " + i+1 + ":");
+						array[i] = read.nextInt();
+				}
+				System.out.println("Your input:");
+				System.out.println(array.toString());
+			}
+			
+			else if (choise==2) {
+				int size = getSize();
+				String[] array = new String[size];
+				for (int i=0 ; i<size ; i++) {
+					System.out.println("Please enter value for place number " + i+1 + ":");
+						array[i] = read.nextLine();
+				}
+				System.out.println("Your input:");
+				System.out.println(array.toString());
+			}
+			else if (choise==-1) {
+				System.out.println("Exiting... Goodbye.");
+				System.exit(0);
+			}
+			else {
+				isValidInput = false;
+				System.out.println("Invalid input!");
+				System.out.println("Choose again from the following:");
+				System.out.println("1. Integer array:");
+				System.out.println("2. String array:");
+				System.out.println("-1 to Exit.");
+			}
+		}
+		
+		
+		
+		
+		
+	}
+	
+	public static int getSize() {
+		Scanner read = new Scanner (System.in);
+		System.out.println("Enter the size of the array you desire:");
+		int size = read.nextInt();
+		return size;
+	}
+	
+	public static <T extends Comparable<T>> void sort(T[] array) {
+		System.out.println("The array before the sort:");
+		System.out.println(array.toString());
+		HeapSort.sort(array);
+		
 	}
 
 }
