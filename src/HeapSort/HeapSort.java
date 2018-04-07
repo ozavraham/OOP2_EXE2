a99478780a2d745f7e093b65fde4a08248d1dc6f
ozavraham <ozavraham@172.20.4.13>
2018-04-07 12:47:39 +0300

    Integer Heapsort is done

--- src/HeapSort/HeapSort.java
+++ src/HeapSort/HeapSort.java
@@ -1,28 +1,29 @@
 package HeapSort;
+import java.util.Arrays;
 
 public class HeapSort {
-	
+
 	static int heapSize=0;
-	
+
 	public static <T extends Comparable<T>> void sort(T[] array) {
 		buildHeap(array);
-		for (int i=array.length ; i>1 ; i--) {
+		for (int i=array.length-1 ; i>1 ; i--) {
 			swap(array, 1, i);
 			HeapSort.heapSize = HeapSort.heapSize-1; 
 			Heapify(array, 1);
 		}
 	}
-	
+
 	private static <T extends Comparable<T>> void buildHeap(T[] array) {
-		heapSize = array.length;
-		for (int i=array.length/2 ; i>=1 ; i--) {
+		HeapSort.heapSize = array.length-1;
+		for (int i=Math.floorDiv(heapSize, 2) ; i>=1 ; i--) {
 			Heapify(array, i);
 		}
 	}
-	
+
 	private static <T extends Comparable<T>> void Heapify(T[] array, int i) {
-		int leftSon = i*2;
-		int rightSon = (2*i)+2;
+		int leftSon = (i*2);
+		int rightSon = (i*2)+1;
 		int largest;
 		if (leftSon<=HeapSort.heapSize && array[leftSon].compareTo(array[i])>0) {
 			largest = leftSon;
@@ -37,9 +38,8 @@ public class HeapSort {
 			swap(array, i, largest);
 			Heapify(array, largest);
 		}
-
 	}
-	
+
 	private static void swap(Object[] array, int i, int j) {
 		Object temp = array[i];
 		array[i] = array[j];
--- src/HeapSort/Main.java
+++ src/HeapSort/Main.java
@@ -8,40 +8,43 @@ public class Main {
 	public static void main(String[] args) {
 		int choise;
 		boolean isValidInput = false;
+		boolean isDone = false;
 		Scanner read = new Scanner(System.in);
 		System.out.println("Welcome to HeapSort Generic Exercise!");
 		System.out.println("Please choose from the following:");
 		System.out.println("1. Integer array");
 		System.out.println("2. String array");
 		System.out.println("-1 to Exit.");
-		while (!isValidInput) {
+		while (!isValidInput && !isDone) {
 			choise = read.nextInt();
 			if (choise==1) {
 				int size = getSize();
-				Integer[] array = new Integer[size];
-				for (int i=0 ; i<size ; i++) {
-					System.out.println("Please enter value for place number " + i + ":");
+				Integer[] array = new Integer[size+1];
+				for (int i=1 ; i<=size ; i++) {
+					System.out.printf("Please enter value for place number %d: ", i);
 					array[i] = read.nextInt();
 				}
 				System.out.println("Your input:");
-				System.out.println(Arrays.toString(array));
+				printArr(array);
 				sort(array);
+				isDone=false;
 			}
 
 			else if (choise==2) {
 				int size = getSize();
-				String[] array = new String[size];
-				for (int i=0 ; i<size ; i++) {
-					System.out.println("Please enter value for place number " + i + ":");
+				String[] array = new String[size+1];
+				for (int i=1 ; i<size ; i++) {
+					System.out.printf("Please enter value for place number %d: ", i);
 					array[i] = read.nextLine();
 				}
 				System.out.println("Your input:");
-				System.out.println(Arrays.toString(array));
+				printArr(array);
 				sort(array);
+				isDone=false;
 			}
 			else if (choise==-1) {
 				System.out.println("Exiting... Goodbye.");
-				System.exit(0);
+				isDone=true;
 			}
 			else {
 				isValidInput = false;
@@ -63,10 +66,22 @@ public class Main {
 
 	public static <T extends Comparable<T>> void sort(T[] array) {
 		System.out.println("The array before the sort:");
-		System.out.println(Arrays.toString(array));
+		printArr(array);
 		HeapSort.sort(array);
 		System.out.println("The array after the sort:");
-		System.out.println(Arrays.toString(array));
+		printArr(array);
+	}
+	
+	public static <T> void printArr(T[] array) {
+		System.out.print("[");
+		for (int i=1 ; i<array.length ; i++) {
+			if (i!=array.length-1) {
+				System.out.print(array[i] + ", ");
+			}
+			else System.out.print(array[i]);
+		}
+		System.out.print("]");
+		System.out.println();
 	}
 
 }
