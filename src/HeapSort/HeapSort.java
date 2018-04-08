4dc10cda248d75c4cb30bf6308cdbf792aa1963d
ozavraham <ozavraham@172.20.4.13>
2018-04-08 19:58:32 +0300

    Done :)

--- src/HeapSort/HeapSort.java
+++ src/HeapSort/HeapSort.java
@@ -4,8 +4,14 @@ public class HeapSort {
 
 	static int heapSize=0;
 
+	/*
+	 * Sort Method
+	 * HeapSort Implementation
+	 * According to the algorithem learnd @ Data Structue class 1'st year
+	 */
 	public static <T extends Comparable<T>> void sort(T[] array) {
-		buildHeap(array);
+		// Sending the array to buildHeap
+		buildHeap(array); 
 		for (int i=array.length-1 ; i>1 ; i--) {
 			swap(array, 1, i);
 			HeapSort.heapSize = HeapSort.heapSize-1; 
@@ -13,6 +19,11 @@ public class HeapSort {
 		}
 	}
 
+	/*
+	 * BuildHeap method
+	 * Building the "Heap" from the original array
+	 * Using Math.floorDiv to get the lower bound from the division
+	 */
 	private static <T extends Comparable<T>> void buildHeap(T[] array) {
 		// If the array is with length 6 with free space at array[0]
 		// Then the heapSize is -1
@@ -22,10 +33,17 @@ public class HeapSort {
 		}
 	}
 
+	/*
+	 * Heaoify method
+	 * Implemnt the Heapify action according to the Min Heap algorithm
+	 */
 	private static <T extends Comparable<T>> void Heapify(T[] array, int i) {
+		// Getting the two sons of the node
 		int leftSon = (i*2);
 		int rightSon = (i*2)+1;
+		// Locating int to save the largest position from them both
 		int largest;
+		// Starting to compare, make sure we are still in the bounderis of the array
 		if (leftSon<=HeapSort.heapSize && array[leftSon].compareTo(array[i])>0) {
 			largest = leftSon;
 		}
@@ -35,12 +53,20 @@ public class HeapSort {
 		if (rightSon<=HeapSort.heapSize && array[rightSon].compareTo(array[largest])>0) {
 			largest = rightSon;
 		}
+		// Makre sure its not the same node!
 		if (largest!=i) {
+			// Making the swap
 			swap(array, i, largest);
+			// Recursive calling
 			Heapify(array, largest);
 		}
 	}
 
+	/*
+	 * Swap method
+	 * Getting the array, the 2 position's needed to be swap
+	 * Swapping the locations
+	 */
 	private static void swap(Object[] array, int i, int j) {
 		Object temp = array[i];
 		array[i] = array[j];
--- src/HeapSort/Main.java
+++ src/HeapSort/Main.java
@@ -1,26 +1,32 @@
 package HeapSort;
-import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
+/* Generic HeapSort Implementation
+ * Aviram Boniel & Oz Avraham
+ * Object Oriented Programming 2
+ */
 
 public class Main {
 
 	public static void main(String[] args) {
-		boolean isValidInput = false;
+		// If the user is done, change to true and exit the while loop
 		boolean isDone = false;
 		Scanner read = new Scanner(System.in);
+		// Displaying menu
 		System.out.println("Welcome to HeapSort Generic Exercise!");
 		System.out.println("Please choose from the following:");
 		System.out.println("1. Integer array");
 		System.out.println("2. String array");
-		System.out.println("-1 to Exit.");
+		System.out.println("-1. to Exit.");
+		// As long as the user ain't finished
 		do {
 			try {
 				int choise = read.nextInt();
 				if (choise==1) {
 					int size = getSize();
 					Integer[] array = new Integer[size+1];
-					System.out.println("You have chosen Integer!");
+					System.out.println("You have chosen Integer's array!");
 					for (int i=1 ; i<=size ; i++) {
 						System.out.printf("Please enter value for place number %d: ", i);
 						array[i] = read.nextInt();
@@ -32,51 +38,69 @@ public class Main {
 						isDone=false;
 						System.out.println("Choose again from the previous menu:");	
 					}
-					else isDone=false;
+					else {
+						System.out.println("Nevermind... Try again!");
+						System.out.println("Choose again from the previous menu:");
+						isDone=false;
+					}
 				}
-
 				else if (choise==2) {
+					System.out.println("You have chosen String's array!");
 					int size = getSize();
 					String[] array = new String[size+1];
-					System.out.println("You have chosen String!");
 					for (int i=1 ; i<=size ; i++) {
 						System.out.printf("Please enter value for place number %d: ", i);
 						array[i] = read.next();
 					}
 					System.out.println("Your input:");
 					printArr(array);
-					sort(array);
-					isDone=false;				
-					System.out.println("Choose again from the previous menu:");
-
+					if (confirmInput()) {
+						sort(array);
+						isDone=false;
+						System.out.println("Choose again from the previous menu:");	
+					}
+					else {
+						System.out.println("Nevermind... Try again!");
+						System.out.println("Choose again from the previous menu:");
+						isDone=false;
+					}
 				}
 				else if (choise==-1) {
 					System.out.println("Exiting... Goodbye.");
 					isDone=true;
 				}
-				else {
-					isValidInput = false;
-					System.out.println("Invalid input!");
-					System.out.println("Choose again from the following:");
-					System.out.println("1. Integer array");
-					System.out.println("2. String array");
-					System.out.println("-1 to Exit.");
-				}
+				else throw new InputMismatchException();
 			}
+			// Catch if the input ain't from the allowed integers
 			catch (InputMismatchException e) {
-				System.out.println("in input exeption");
+				System.out.println("Invalid input!");
+				System.out.println("Choose again from the following:");
+				System.out.println("1. Integer array");
+				System.out.println("2. String array");
+				System.out.println("-1. to Exit.");
+				read.nextLine();
+				isDone=false;
 			}
-		}while (!isValidInput && !isDone);
+		}while (!isDone);
+		// Closing scanner
 		read.close();
 	}
 
-	public static int getSize() { //java.util.InputMismatchException
+	/*
+	 * Getting the desired size for the array
+	 * Throws InputMismatchException in case of illigeal input
+	 */
+	public static int getSize() throws InputMismatchException{ 
 		Scanner read = new Scanner (System.in);
 		System.out.println("Enter the size of the array you desire:");
 		int size = read.nextInt();
 		return size;
 	}
-
+	
+	/*
+	 * Getting the generic array
+	 * Printing the original array, sending it to Sort @ HeapSort class and printing it after the HeapSort
+	 */
 	public static <T extends Comparable<T>> void sort(T[] array) {
 		System.out.println("The array before the sort:");
 		printArr(array);
@@ -84,7 +108,10 @@ public class Main {
 		System.out.println("The array after the sort:");
 		printArr(array);
 	}
-
+	
+	/*
+	 * Simple 'for' loop to print the array
+	 */
 	public static <T> void printArr(T[] array) {
 		System.out.print("[");
 		for (int i=1 ; i<array.length ; i++) {
@@ -97,25 +124,21 @@ public class Main {
 		System.out.println();
 	}
 
-	public static boolean confirmInput() {
-		try {
-			Scanner read = new Scanner(System.in);
-			String input;
-			boolean bool;
-			System.out.println("Should we continue to sort? [Y/N]");
-			input = read.next();
-			input.toLowerCase();
-			if (input.equals("y")) bool = true;
-			else if (input.equals("n")) bool =false;
-			else throw new InputMismatchException ();
-			return bool;
-		}
-		catch (InputMismatchException e) {
-			System.out.println("Invalid Input!");
-			System.out.println("Please try again!");
-			confirmInput();
-		}
-		return true;
+	/*
+	 * Confirmation with the user that he want to continue
+	 * Throws InputMismatchException in case of illigeal input
+	 */
+	public static boolean confirmInput() throws InputMismatchException{
+		Scanner read = new Scanner(System.in);
+		String input;
+		boolean bool;
+		System.out.println("Should we continue to sort? [Y/N]");
+		input = read.next();
+		input.toLowerCase();
+		if (input.equals("y")) bool = true;
+		else if (input.equals("n")) bool =false;
+		else throw new InputMismatchException ();
+		return bool;
 	}
 
 }
